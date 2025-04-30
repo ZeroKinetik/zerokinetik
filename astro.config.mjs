4d7db608fdab56c0bce457e061d1057f7a37be2f
ZeroKinetik <hemansakkthivelms14@gmail.com>
2025-04-30 11:49:54 +0530

    about-updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://zerokinetik.pages.dev",
+  output: "server", // Use 'server' for SSR or 'static' for static site generation
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap(), tailwind()],
 });
